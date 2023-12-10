276c0bac3d159a7844d016e8f8e97821fa343250
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-12-10 20:36:20 -0300

    feat check winner

--- script.js
+++ script.js
@@ -1,91 +1,65 @@
-const BOARD_MANAGER = (function () {
-  const NEW_BOARD = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
+const boardManager = (function () {
+  const NEWBOARD = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
   ];
 
-  const CREATE_BOARD = function () {
-    return NEW_BOARD;
-  };
+  const CREATE_BOARD = () => NEWBOARD;
 
-  let LOG_BOARD = function (board) {
+  const LOG_BOARD = (board) =>
     console.log(`
     ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
-  ----+---+-----
+    --+---+--
     ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
-  ----+---+-----
+    --+---+--
     ${board[2][0]} | ${board[2][1]} | ${board[2][2]}
     `);
+
+  const placePiece = function (board, piece, position) {
+    board[position[0]][position[1]] = piece;
+    CHECK_WINNER(board);
   };
 
-  let PLACE_PIECE = function (board, player, position) {
-    board[position[0]][position[1]] = player.piece;
-  };
+  const CHECK_WINNER = function (board) {
+    const WIN_CONDITIONS = [
+      [board[0][0], board[0][1], board[0][2]], //Row 1
+      [board[1][0], board[1][1], board[1][2]], //Row 2
+      [board[2][0], board[2][1], board[2][2]], //Row 3
+      [board[0][0], board[1][0], board[2][0]], //Collumn 1
+      [board[0][1], board[1][1], board[2][1]], //Collumn 2
+      [board[0][2], board[1][2], board[2][2]], //Collumn 3
+      [board[0][0], board[1][1], board[2][2]], //Diagonal \
+      [board[2][0], board[1][1], board[0][2]], //Diagonal /
+    ];
 
-  const CHECK_WINNER = function(board){
-    board.forEach(row => {
-        const hasWinner = row.every((cell) => {
-            return (cell == "X" || cell == "O") && cell != " ";
-        })
-        if (hasWinner) {
-            console.log("HAS WINNER");
-        }
-    });
-  }
-
-  return { CREATE_BOARD, LOG_BOARD, PLACE_PIECE, CHECK_WINNER};
-})();
-
-const PLAYER_MANAGER = (function () {
-  let player1 = {
-    name: "Player 1",
-    piece: "X",
-  };
-  let player2 = {
-    name: "Player 2",
-    piece: "O",
-  };
-
-  const getPlayer = function (player) {
-    return player;
-  };
-
-  const setPlayer = function (player, name) {
-    player.name = name;
-  };
-
-  return { getPlayer, setPlayer };
-})();
-
-const GAME_MANAGER = function () {
-  let board = BOARD_MANAGER.CREATE_BOARD();
-  let playerTurn = true;
-  let position = [];
-
-  BOARD_MANAGER.LOG_BOARD(board);
-
-  let whosNext = function (player) {
-    playerTurn = !player;
-    return !playerTurn; //Invert boolean value, Player1 = TRUE / Player2 = FALSE
-  };
-
-  const playRound = () => {
-    for (let i = 0; i < 5; i++) {
-      position[0] = +prompt("Which row?");
-      position[1] = +prompt("Which collumn?");
-      BOARD_MANAGER.PLACE_PIECE(
-        board,
-        PLAYER_MANAGER.getPlayer(playerTurn ? player3 : player2),
-        position
+    WIN_CONDITIONS.forEach((row) => {
+      let hasWinner = row.every(
+        (element) => element === "X" || element === "O"
       );
-      whosNext(playerTurn);
-      BOARD_MANAGER.LOG_BOARD(board);
-      BOARD_MANAGER.CHECK_WINNER(board);
-    }
+      if (hasWinner) {
+        console.log(row);
+        LOG_BOARD(board);
+        return;
+      }
+    });
   };
+  return {
+    CREATE_BOARD,
+    LOG_BOARD,
+    placePiece,
+    CHECK_WINNER,
+  };
+})();
 
-  playRound();
-};
+// let board = boardManager.CREATE_BOARD();
 
-GAME_MANAGER();
+// boardManager.LOG_BOARD(board);
+
+// boardManager.placePiece(board, "X", [0, 2])
+// boardManager.placePiece(board, "X", [0, 0])
+// boardManager.placePiece(board, "X", [0, 1])
+
+// boardManager.placePiece(board, "O", [2, 2])
+// boardManager.placePiece(board, "O", [2, 0])
+// boardManager.placePiece(board, "O", [2, 1])
