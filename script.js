e2af98756951348ae23f4272820d31c48167a58c
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-12-10 21:55:57 -0300

    feat functional game
    need to add cell checking for empty cells

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const boardManager = (function () {
+const BOARD_MANAGER = (function () {
   const NEWBOARD = [
     ["1", "2", "3"],
     ["4", "5", "6"],
@@ -18,7 +18,6 @@ const boardManager = (function () {
 
   const placePiece = function (board, piece, position) {
     board[position[0]][position[1]] = piece;
-    CHECK_WINNER(board);
   };
 
   const CHECK_WINNER = function (board) {
@@ -33,16 +32,17 @@ const boardManager = (function () {
       [board[2][0], board[1][1], board[0][2]], //Diagonal /
     ];
 
-    WIN_CONDITIONS.forEach((row) => {
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const row = WIN_CONDITIONS[i];
       let hasWinner = row.every(
-        (element) => element === "X" || element === "O"
+        (element) => element === row[0] && element !== " "
       );
       if (hasWinner) {
-        console.log(row);
         LOG_BOARD(board);
-        return;
+        return true;
       }
-    });
+    }
+    return false;
   };
   return {
     CREATE_BOARD,
@@ -52,14 +52,68 @@ const boardManager = (function () {
   };
 })();
 
-// let board = boardManager.CREATE_BOARD();
+const PLAYER_MANAGER = (function () {
+  let player1 = {
+    name: "Player 1",
+    piece: "X",
+  };
+  let player2 = {
+    name: "Player 2",
+    piece: "O",
+  };
 
-// boardManager.LOG_BOARD(board);
+  const getPlayer1 = () => player1;
+  const getPlayer2 = () => player2;
 
-// boardManager.placePiece(board, "X", [0, 2])
-// boardManager.placePiece(board, "X", [0, 0])
-// boardManager.placePiece(board, "X", [0, 1])
+  return {
+    getPlayer1,
+    getPlayer2,
+  };
+})();
 
-// boardManager.placePiece(board, "O", [2, 2])
-// boardManager.placePiece(board, "O", [2, 0])
-// boardManager.placePiece(board, "O", [2, 1])
+const GAME_MANAGER = (function () {
+  let currentPlayer = PLAYER_MANAGER.getPlayer1();
+
+  const changeTurn = function () {
+    currentPlayer =
+      currentPlayer === PLAYER_MANAGER.getPlayer1()
+        ? PLAYER_MANAGER.getPlayer2()
+        : PLAYER_MANAGER.getPlayer1();
+  };
+
+  const playGame = function () {
+    let board = BOARD_MANAGER.CREATE_BOARD();
+
+    BOARD_MANAGER.LOG_BOARD(board);
+    let position = [];
+    for (let i = 0; i < 9; i++) {
+      position[0] = parseInt(prompt("Row: "));
+      position[1] = parseInt(prompt("Column: "));
+
+      BOARD_MANAGER.placePiece(board, currentPlayer.piece, position);
+      BOARD_MANAGER.LOG_BOARD(board);
+
+      if (BOARD_MANAGER.CHECK_WINNER(board)) {
+        console.log(`${currentPlayer.name} has won the game!`);
+        break;
+      }
+      changeTurn();
+    }
+  };
+
+  return { playGame };
+})();
+
+GAME_MANAGER.playGame();
+
+// let board = BOARD_MANAGER.CREATE_BOARD();
+
+// BOARD_MANAGER.LOG_BOARD(board);
+
+// BOARD_MANAGER.placePiece(board, "X", [0, 2])
+// BOARD_MANAGER.placePiece(board, "0", [0, 0])
+// BOARD_MANAGER.placePiece(board, "X", [0, 1])
+
+// BOARD_MANAGER.placePiece(board, "O", [2, 2])
+// BOARD_MANAGER.placePiece(board, "O", [2, 0])
+// BOARD_MANAGER.placePiece(board, "O", [2, 1])
