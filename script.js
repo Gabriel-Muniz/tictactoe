6d3b875d8250a7f7694ba610916ebf1bf69f5fd0
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-12-13 21:53:46 -0300

    refac code

--- script.js
+++ script.js
@@ -1,139 +1,66 @@
 const BOARD_MANAGER = (function () {
-  const NEWBOARD = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
-  ];
+  const NEW_BOARD = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
-  const CREATE_BOARD = () => NEWBOARD;
+  let currentBoard = [];
 
-  const LOG_BOARD = (board) =>
+  const CREATE_BOARD = () => {
+    currentBoard = NEW_BOARD;
+  };
+
+  const LOG_BOARD = function () {
     console.log(`
-    ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
+    ${currentBoard[0]} | ${currentBoard[1]} | ${currentBoard[2]}
     --+---+--
-    ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
+    ${currentBoard[3]} | ${currentBoard[4]} | ${currentBoard[5]}
     --+---+--
-    ${board[2][0]} | ${board[2][1]} | ${board[2][2]}
+    ${currentBoard[6]} | ${currentBoard[7]} | ${currentBoard[8]}
     `);
-
-  const placePiece = function (board, piece, position) {
-    board[position[0]][position[1]] = piece;
   };
 
-  const CHECK_WINNER = function (board) {
-    const WIN_CONDITIONS = [
-      [board[0][0], board[0][1], board[0][2]], //Row 1
-      [board[1][0], board[1][1], board[1][2]], //Row 2
-      [board[2][0], board[2][1], board[2][2]], //Row 3
-      [board[0][0], board[1][0], board[2][0]], //Collumn 1
-      [board[0][1], board[1][1], board[2][1]], //Collumn 2
-      [board[0][2], board[1][2], board[2][2]], //Collumn 3
-      [board[0][0], board[1][1], board[2][2]], //Diagonal \
-      [board[2][0], board[1][1], board[0][2]], //Diagonal /
-    ];
-
-    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
-      const row = WIN_CONDITIONS[i];
-      let hasWinner = row.every(
-        (element) => element === row[0] && element !== " "
-      );
-      if (hasWinner) {
-        LOG_BOARD(board);
-        return true;
-      }
-    }
-    return false;
+  const GET_BOARD = function () {
+    return currentBoard;
   };
 
-  const CHECK_CELL = function (board, position) {
-    if (board[position[0]][position[1]] !== " ") {
-        alert("You have to choose a empty cell!");
-        return false;
-    }
-    return true;
-  }
-  return {
-    CREATE_BOARD,
-    LOG_BOARD,
-    placePiece,
-    CHECK_WINNER,
-    CHECK_CELL
+  const placePiece = function (position, piece) {
+    currentBoard[position] = piece;
   };
+  return { CREATE_BOARD, LOG_BOARD, placePiece, GET_BOARD };
 })();
 
 const PLAYER_MANAGER = (function () {
-  let player1 = {
-    name: "Player 1",
-    piece: "X",
-  };
-  let player2 = {
-    name: "Player 2",
-    piece: "O",
-  };
+  class Player{
+    constructor(name, piece){
+      this.name = name;
+      this.piece = piece;
+    }
 
-  const getPlayer1 = () => player1;
-  const getPlayer2 = () => player2;
+    get name(){
+      return this._name;
+    }
 
-  return {
-    getPlayer1,
-    getPlayer2,
-  };
+    get piece(){
+      return this._piece;
+    }
+
+    set name(name){
+      this._name = name;
+    }
+
+    set piece(piece){
+      this._piece = piece;
+    }
+  }
+
+  let player1 = new Player("Player 1", "X");
+  let player2 = new Player("Player 2", "O");
+
+  return{
+    player1,
+    player2
+  }
 })();
 
-/* const GAME_MANAGER = (function () {
-  let currentPlayer = PLAYER_MANAGER.getPlayer1();
-
-  const changeTurn = function () {
-    currentPlayer =
-      currentPlayer === PLAYER_MANAGER.getPlayer1()
-        ? PLAYER_MANAGER.getPlayer2()
-        : PLAYER_MANAGER.getPlayer1();
-  };
-
-  const playGame = function () {
-    let board = BOARD_MANAGER.CREATE_BOARD();
-
-    BOARD_MANAGER.LOG_BOARD(board);
-    let position = [];
-    for (let i = 0; i < 9; i++) {
-      do {
-        position[0] = parseInt(prompt("Row: "));
-        position[1] = parseInt(prompt("Column: "));
-      } while (!BOARD_MANAGER.CHECK_CELL(board, position));
-
-        
-      BOARD_MANAGER.placePiece(board, currentPlayer.piece, position);
-      BOARD_MANAGER.LOG_BOARD(board);
-
-      if (BOARD_MANAGER.CHECK_WINNER(board)) {
-        console.log(`${currentPlayer.name} has won the game!`);
-        break;
-      }
-      changeTurn();
-    }
-  };
-
-  return { playGame };
-})(); */
-
-const UI_MANAGER = (function() {
-  const boardCell = document.querySelectorAll(".board-cell");
-  boardCell.forEach(cell => {
-    cell.addEventListener("click", () => {
-      console.log(cell);
-    })
-  })
-})()
-
-
-// let board = BOARD_MANAGER.CREATE_BOARD();
-
-// BOARD_MANAGER.LOG_BOARD(board);
-
-// BOARD_MANAGER.placePiece(board, "X", [0, 2])
-// BOARD_MANAGER.placePiece(board, "0", [0, 0])
-// BOARD_MANAGER.placePiece(board, "X", [0, 1])
-
-// BOARD_MANAGER.placePiece(board, "O", [2, 2])
-// BOARD_MANAGER.placePiece(board, "O", [2, 0])
-// BOARD_MANAGER.placePiece(board, "O", [2, 1])
+BOARD_MANAGER.CREATE_BOARD();
+BOARD_MANAGER.LOG_BOARD();
+BOARD_MANAGER.placePiece(1, PLAYER_MANAGER.player1.piece);
+BOARD_MANAGER.LOG_BOARD();
