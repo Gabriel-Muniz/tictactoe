98c021402b739699548d23f79ced3d2797ce3b4d
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-12-13 22:18:27 -0300

    feat winner checking

--- script.js
+++ script.js
@@ -28,25 +28,25 @@ const BOARD_MANAGER = (function () {
 })();
 
 const PLAYER_MANAGER = (function () {
-  class Player{
-    constructor(name, piece){
+  class Player {
+    constructor(name, piece) {
       this.name = name;
       this.piece = piece;
     }
 
-    get name(){
+    get name() {
       return this._name;
     }
 
-    get piece(){
+    get piece() {
       return this._piece;
     }
 
-    set name(name){
+    set name(name) {
       this._name = name;
     }
 
-    set piece(piece){
+    set piece(piece) {
       this._piece = piece;
     }
   }
@@ -54,13 +54,46 @@ const PLAYER_MANAGER = (function () {
   let player1 = new Player("Player 1", "X");
   let player2 = new Player("Player 2", "O");
 
-  return{
+  return {
     player1,
-    player2
-  }
+    player2,
+  };
+})();
+
+const GAME_MANAGER = (function () {
+  const CHECK_WINNER = () => {
+    let board = BOARD_MANAGER.GET_BOARD();
+    const WIN_CONDITIONS = [
+      [board[0], board[1], board[2]], //First row
+      [board[3], board[4], board[5]], //Second row
+      [board[6], board[7], board[8]], //Third row
+      [board[0], board[3], board[6]], //First column
+      [board[1], board[4], board[7]], //Second column
+      [board[2], board[5], board[8]], //Third column
+      [board[0], board[4], board[8]], //Diagonal \
+      [board[2], board[4], board[6]], //Diagonal /
+    ];
+
+    let hasWinner = false;
+
+    WIN_CONDITIONS.forEach((row) => {
+      if (row.every((cell) => cell === row[0] && cell !== " ")) { //If every cell in a row(WIN_CONDITIONS row) be equal then return true;
+        hasWinner = true;
+        return;
+      }
+    });
+    return hasWinner;
+  };
+
+  return {
+    CHECK_WINNER,
+  };
 })();
 
 BOARD_MANAGER.CREATE_BOARD();
 BOARD_MANAGER.LOG_BOARD();
-BOARD_MANAGER.placePiece(1, PLAYER_MANAGER.player1.piece);
+BOARD_MANAGER.placePiece(6, PLAYER_MANAGER.player1.piece);
+BOARD_MANAGER.placePiece(2, PLAYER_MANAGER.player1.piece);
+BOARD_MANAGER.placePiece(4, PLAYER_MANAGER.player1.piece);
 BOARD_MANAGER.LOG_BOARD();
+console.log(GAME_MANAGER.CHECK_WINNER());
