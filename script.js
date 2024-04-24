3441bab05c17e8a29d10e781cd95cec68ef9d92e
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-04-23 21:34:35 -0300

    feat placing pieces into board

--- script.js
+++ script.js
@@ -5,21 +5,59 @@ const BOARD_CONTROLLER = (function () {
     for (let i = 0; i < 3; i++) {
       board[i] = [];
       for (let j = 0; j < 3; j++) {
-        board[i].push('0');
+        board[i].push(j);
       }
     }
   }
+
+  const getBoard = () => board;
 
   const LOG_BOARD = () => board.forEach(row => {
     console.log(`${row[0]} | ${row[1]} | ${row[2]}`);
     console.log('-----')
   });
 
-  const PLACE_PIECE = (row, column) => {
-    console.log('Placing piece');
+  const PLACE_PIECE = (row, column, piece) => {
+    console.log(row, column);
+    board[row][column] = piece;
   }
-  return {CREATE_BOARD, LOG_BOARD, PLACE_PIECE}
+  return {CREATE_BOARD, getBoard, LOG_BOARD, PLACE_PIECE}
+})()
+
+const GAME_CONTROLLER = (function () {
+  const players = {
+    playerOne: {
+      id: 1,
+      name: 'Player One',
+      mark: 'X',
+    },
+    playerTwo: {
+      id: 2,
+      name: 'Player two',
+      mark: 'O',
+    }
+  }
+
+  let currentPlayer = players.playerOne;
+
+  const CHANGE_TURN = () => currentPlayer = (currentPlayer === players.playerOne) ? players.playerTwo : players.playerOne;
+  const PLAY_ROUND = () => {
+    const chooseRow = Number(prompt('Linha?'));
+    const chooseColumn = Number(prompt('Coluna?'));
+    BOARD_CONTROLLER.PLACE_PIECE(chooseRow, chooseColumn, currentPlayer.mark);
+  };
+
+  const WIN_CONDITIONS = []
+
+  const CHECK_WINNER = () => {
+    gameboard = BOARD_CONTROLLER.getBoard();
+    rowChecking = (element) => element
+
+    gameboard.forEach(row => row.every())
+  }
+
+  return{CHANGE_TURN, PLAY_ROUND}
 })()
 
 BOARD_CONTROLLER.CREATE_BOARD();
-BOARD_CONTROLLER.LOG_BOARD();
+GAME_CONTROLLER.PLAY_ROUND();
