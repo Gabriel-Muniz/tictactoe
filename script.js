2541ee6c12934b6d0cd1a487ef54a7f5ed0339dd
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-05-27 20:25:04 -0300

    dont remember sorry

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Board
 const boardManager = (function () {
-  let gameboard = [2,1,2,1,1,2,2,2,0]; // Cria o novo tabuleiro
+  let gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // Cria o novo tabuleiro
 
   const logBoard = () => {
     console.log(`
@@ -38,30 +38,30 @@ const gameManager = (function () {
     players.currentPlayer = players.currentPlayer == 1 ? 2 : 1;
   };
 
-  const playRound = () => {
-    let play;
+  const playRound = (index) => {
+    /*const play;
 
-    do {
+     do {
       play = prompt("Where?");
-    } while (play < 0 || play > 8 || boardManager.getBoard()[play] != 0);
+    } while (play < 0 || play > 8 || boardManager.getBoard()[play] != 0); */
 
-    boardManager.makeMove(players.currentPlayer, play);
+    boardManager.makeMove(players.currentPlayer, index);
     boardManager.logBoard();
   };
 
   const playGame = () => {
     let board = boardManager.getBoard();
-    let fullBoard = ((item) => item != 0); //Condição booleana para o método de Array every.
-    while(!board.every(fullBoard)){//Enquanto todo o tabuleiro não for diferente de 0(Casa vazia) ou termos um vencedor continue
+    let fullBoard = (item) => item != 0; //Condição booleana para o método de Array every.
+    while (!board.every(fullBoard)) {
+      //Enquanto todo o tabuleiro não for diferente de 0(Casa vazia) ou termos um vencedor continue
       playRound();
 
-      if(checkWinner()) return;
-
-      changeTurn();
+      if (checkWinner()) break;
     }
     if (board.every(fullBoard)) {
-      console.log(`Woomp Woomp that's a draw`)
+      console.log(`Woomp Woomp that's a draw`);
     }
+    resetGame();
   };
 
   const checkWinner = () => {
@@ -74,7 +74,7 @@ const gameManager = (function () {
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
-    ];//Array contendo as condições de vitórias armazendo posições do array, que ao serem dispostas da forma correta formarão um tabuleiro
+    ]; //Array contendo as condições de vitórias armazendo posições do array, que ao serem dispostas da forma correta formarão um tabuleiro
 
     const board = boardManager.getBoard();
 
@@ -83,12 +83,77 @@ const gameManager = (function () {
     );
     //Método find ira array por array e rodará every nos mesmos até que encontre uma condição de vitória satisfatória
 
-    if(winner){
+    if (winner) {
       console.log(`Player ${players.currentPlayer} is the winner!`);
     }
     return winner;
   };
-  return { changeTurn, players, playRound, playGame };
+
+  const resetGame = () => {
+    const answer = prompt("Play gain?");
+    if (answer) {
+      boardManager.resetBoard();
+      players.currentPlayer = 1;
+    }
+  };
+
+  return { changeTurn, players, playRound, playGame, resetGame, checkWinner, changeTurn };
 })();
 
 // Fazer o loop do jogo em si, com as 9 rodadas e entrar na DOM em seguida
+
+const domManager = (function () {
+  const boardElem = document.querySelector(".board");
+  const board = boardManager.getBoard;
+
+  const initBoard = () => {
+    board().map((cell, index) => {
+      const cellDiv = document.createElement("div");
+      cellDiv.classList.add("cell-div");
+      cellDiv.dataset.index = index;
+      boardElem.append(cellDiv);
+    });
+
+    cellClick();
+  };
+
+  const cellClick = () => {
+    const allCellDiv = document.querySelectorAll(".cell-div");
+
+    allCellDiv.forEach((cellDiv) => {
+      cellDiv.addEventListener("click", () => {
+        const divIndex = cellDiv.getAttribute("data-index");
+
+        if (board()[divIndex]) {
+          //Caso a célula selecionada seja true(esteja ocupada) retorne;
+          return;
+        }
+        gameManager.playRound(divIndex);
+
+        if(gameManager.checkWinner()){
+          alert(gameManager.checkWinner());
+        };
+
+        updateBoard();
+
+        gameManager.changeTurn();
+
+      });
+    });
+  };
+
+  const updateBoard = () => {
+    const allCellDiv = document.querySelectorAll(".cell-div");
+
+    board().map((cell, index) => {
+      if (cell == 0) {
+        return;
+      }
+      allCellDiv[index].textContent = cell == 1 ? "X" : "O";
+    });
+  };
+
+  return { initBoard };
+})();
+
+domManager.initBoard();
