2f81ceda36852017b06ebeccf2dac200c6e9305e
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-16 20:33:03 -0300

    :sparkles:FEAT: Add board manager

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('it works')
+console.log("it works");
+
+/* 
+  Criar tabuleiro
+
+  Retornar array de 9 casas (0 - 8)
+*/
+
+const BOARD_MANAGER = (function () {
+
+  const createBoard = () => ["X", "O", "X", "O", "X", "O", "O", "X", "X"];
+  board = createBoard();
+
+  const getBoard = () => board[0];
+
+  const logBoard = function () {
+    console.log(`
+      ${board[0]} ${board[1]} ${board[2]}
+      ${board[3]} ${board[4]} ${board[5]}
+      ${board[6]} ${board[7]} ${board[8]}
+      
+    `);
+  };
+
+  return { getBoard, createBoard, logBoard };
+})();
+
+/*
+  Players serão objetos assim como o controlador do jogo
+*/
