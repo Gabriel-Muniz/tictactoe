d1d16230638f331982616dcef4d9b1022b455b6f
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-16 21:23:16 -0300

    :sparkles:FEAT: Add playerManager

--- script.js
+++ script.js
@@ -25,6 +25,28 @@ const BOARD_MANAGER = (function () {
   return { getBoard, createBoard, logBoard };
 })();
 
+const PLAYER_MANAGER = (function(){
+  const players = {
+    player1: {
+      name: 'Player 1',
+      sign: 'X',
+    },
+    player2: {
+      name: 'Player 2',
+      sign: 'O',
+    }
+  }
+
+  const changePlayerName = (jogador, newName) => {
+    players[jogador].name = newName;
+  }
+
+  const logPlayer = (player) => {
+    console.log(players[player]);
+  }
+
+  return {changePlayerName, logPlayer}
+})()
 /*
   Players serão objetos assim como o controlador do jogo
 */
