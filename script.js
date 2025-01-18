e2b3da9fd1ca14b459cdf24dade5d0d05ab6d87e
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-18 11:40:30 -0300

    :sparkles:FEAT: Add checkWinner and players turn

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ console.log("it works");
 
 const BOARD_MANAGER = (function () {
   const createBoard = () => ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
-  board = createBoard();
+  let board = createBoard();
 
-  const getBoard = () => board[0];
+  const getBoard = () => board;
 
   const logBoard = function () {
     console.log(`
@@ -53,9 +53,43 @@ const PLAYER_MANAGER = (function () {
   return { changePlayerName, logPlayer, getPlayer };
 })();
 
+const GAME_MANAGER = (function () {
+  let currentPlayer = PLAYER_MANAGER.getPlayer("player1");
+
+  const changePlayerTurn =
+    currentPlayer === PLAYER_MANAGER.getPlayer("player1")
+      ? PLAYER_MANAGER.getPlayer("player2")
+      : PLAYER_MANAGER.getPlayer("player1");
+
+  const checkWinner = (board) => {
+    const winConditions = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    for (const winGroup of winConditions) {
+      if (winGroup.every((position) => position === currentPlayer.sign)) {
+        console.log(`${currentPlayer.name} é o ganhador!`);
+        break;
+      }
+    }
+  };
+
+  return { checkWinner, changePlayerTurn};
+})();
+
 /* Zona de teste */
 PLAYER_MANAGER.logPlayer("player1");
 PLAYER_MANAGER.changePlayerName("player1", "Gabriel");
 PLAYER_MANAGER.logPlayer("player1");
-BOARD_MANAGER.placePiece(PLAYER_MANAGER.getPlayer('player1'), 3);
+BOARD_MANAGER.placePiece(PLAYER_MANAGER.getPlayer("player1"), 0);
+BOARD_MANAGER.placePiece(PLAYER_MANAGER.getPlayer("player1"), 4);
+BOARD_MANAGER.placePiece(PLAYER_MANAGER.getPlayer("player1"), 8);
 BOARD_MANAGER.logBoard();
+GAME_MANAGER.checkWinner(BOARD_MANAGER.getBoard());
